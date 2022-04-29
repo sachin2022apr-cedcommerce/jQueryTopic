db136165aabfc98872656e3b8614b7e013066ad7
sachin2022apr-cedcommerce <sachin2022apr@cedcoss.com>
2022-04-29 17:37:51 +0530

    Dropdown

--- jsTask1.js
+++ jsTask1.js
@@ -1,21 +1,11 @@
-function productList() {
-    /*let id = document.getElementById("PId").value;  
-    let name = document.getElementById("PName").value; 
-    let price = document.getElementById("PPrice").value; 
-    */
-    var product=[];
+var product=[];
     product[0]={Company:"Samsung",Model:"Galaxy",Memory:"64",Price:"15000"};
     product[1]={Company:"Nokia",Model:"S730",Memory:"128",Price:"22000"};
     product[2]={Company:"Xiaomi",Model:"Note",Memory:"32",Price:"12000"};
     product[3]={Company:"Motoroala",Model:"G10",Memory:"32",Price:"15000"};
     product[4]={Company:"Apple",Model:"S12",Memory:"64",Price:"25000"};
-    /*var cell1 = row.insertCell(0);
-    var cell2 = row.insertCell(1);
-    var cell3 = row.insertCell(2);
-    cell1.innerHTML = id;
-    cell2.innerHTML = n;
-    cell3.innerHTML = price;
-   */
+
+function productList() {
     for(var i=0;i<=product.length;){
         var row = myTable.insertRow();
         
@@ -31,4 +21,78 @@ function productList() {
         
     }
 }
+function filterProduct(){
+    var Table = document.getElementById("table2");
+    Table.innerHTML = "";
 
+    var f = document.getElementById("filter").value;  
+    
+    var s = document.getElementById("search").value;
+    var product=[];
+    product[0]={Company:"Samsung",Model:"Galaxy",Memory:"64",Price:"15000"};
+    product[1]={Company:"Nokia",Model:"S730",Memory:"128",Price:"22000"};
+    product[2]={Company:"Xiaomi",Model:"Note",Memory:"32",Price:"12000"};
+    product[3]={Company:"Motoroala",Model:"G10",Memory:"32",Price:"15000"};
+    product[4]={Company:"Apple",Model:"S12",Memory:"64",Price:"25000"};
+
+    for(var i=0;i<=product.length;i++){
+            if(f=="Company"){
+                if(product[i].Company==s){
+                    var row = table2.insertRow();
+                    var cell1 = row.insertCell(0);
+                    cell1.innerHTML = product[i].Company;
+                    var cell2 = row.insertCell(1);
+                    cell2.innerHTML = product[i].Model;
+                    var cell3 = row.insertCell(2);
+                    cell3.innerHTML = product[i].Memory;
+                    var cell4 = row.insertCell(3);
+                    cell4.innerHTML = product[i].Price;
+                    
+                }
+            }
+            if(f=="Model"){
+                if(product[i].Model==s){
+                    var row = table2.insertRow();
+                    var cell1 = row.insertCell(0);
+                    cell1.innerHTML = product[i].Company;
+                    var cell2 = row.insertCell(1);
+                    cell2.innerHTML = product[i].Model;
+                    var cell3 = row.insertCell(2);
+                    cell3.innerHTML = product[i].Memory;
+                    var cell4 = row.insertCell(3);
+                    cell4.innerHTML = product[i].Price;
+                    
+                }
+            }
+            if(f=="Memory"){
+                if(product[i].Memory==s){
+                    var row = table2.insertRow();
+                    var cell1 = row.insertCell(0);
+                    cell1.innerHTML = product[i].Company;
+                    var cell2 = row.insertCell(1);
+                    cell2.innerHTML = product[i].Model;
+                    var cell3 = row.insertCell(2);
+                    cell3.innerHTML = product[i].Memory;
+                    var cell4 = row.insertCell(3);
+                    cell4.innerHTML = product[i].Price;
+                    
+                }
+            }
+            if(f=="Price"){
+                if(product[i].Price==s){
+                    var row = table2.insertRow();
+                    var cell1 = row.insertCell(0);
+                    cell1.innerHTML = product[i].Company;
+                    var cell2 = row.insertCell(1);
+                    cell2.innerHTML = product[i].Model;
+                    var cell3 = row.insertCell(2);
+                    cell3.innerHTML = product[i].Memory;
+                    var cell4 = row.insertCell(3);
+                    cell4.innerHTML = product[i].Price;
+                    
+                }
+            }
+            
+        
+    }
+}
