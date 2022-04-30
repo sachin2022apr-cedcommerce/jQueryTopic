e0b416b305111eb3bd08c9400b0f80a3555d18ac
sachin2022apr-cedcommerce <sachin2022apr@cedcoss.com>
2022-04-30 12:02:18 +0530

    Add product to the list

--- jsTask1.js
+++ jsTask1.js
@@ -1,24 +1,18 @@
 var product=[];
-    product[0]={Company:"Samsung",Model:"Galaxy",Memory:"64",Price:"15000"};
-    product[1]={Company:"Nokia",Model:"S730",Memory:"128",Price:"22000"};
-    product[2]={Company:"Xiaomi",Model:"Note",Memory:"32",Price:"12000"};
-    product[3]={Company:"Motoroala",Model:"G10",Memory:"32",Price:"15000"};
-    product[4]={Company:"Apple",Model:"S12",Memory:"64",Price:"25000"};
-    function compare( a, b ) {
-        if ( a.Company < b.Company ){
-          return -1;
-        }
-        if ( a.Company > b.Company ){
-          return 1;
-        }
-        return 0;
-      }
-      product.sort( compare );
-      console.log(product);
+    
+product[0]={Company:"Samsung",Model:"Galaxy",Memory:64,Price:1500};
+    
+product[1]={Company:"Nokia",Model:"S730",Memory:128,Price:220};
+    
+product[2]={Company:"Xiaomi",Model:"Note",Memory:32,Price:12000};
+    
+product[3]={Company:"Motoroala",Model:"G10",Memory:32,Price:1500};
+    
+product[4]={Company:"Apple",Model:"S12",Memory:64,Price:25000};
+    
 
 
-
-function productList() {
+  function productList() {
     for(var i=0;i<=product.length;){
         var row = table1.insertRow();
         
@@ -37,20 +31,16 @@ function productList() {
     function newFunction() {
         cell1.innerHTML = product[i].Company;
     }
-}
-function filterProduct(){
+  }
+
+
+  function filterProduct(){
     var Table = document.getElementById("table2");
     Table.innerHTML = "";
 
     var f = document.getElementById("filter").value;  
     
     var s = document.getElementById("search").value;
-    var product=[];
-    product[0]={Company:"Samsung",Model:"Galaxy",Memory:"64",Price:"15000"};
-    product[1]={Company:"Nokia",Model:"S730",Memory:"128",Price:"22000"};
-    product[2]={Company:"Xiaomi",Model:"Note",Memory:"32",Price:"12000"};
-    product[3]={Company:"Motoroala",Model:"G10",Memory:"32",Price:"15000"};
-    product[4]={Company:"Apple",Model:"S12",Memory:"64",Price:"25000"};
 
     for(var i=0;i<=product.length;i++){
             if(f=="Company"){
@@ -112,9 +102,9 @@ function filterProduct(){
             
         
     }
-}
+  }
 
-function sortProduct(){
+  function sortProduct(){
     var Table = document.getElementById("table3");
     Table.innerHTML = "";
 
@@ -174,10 +164,10 @@ function sortProduct(){
 
                 if(option=="Memory"){
                     function compare( a, b ) {
-                        if ( a.Memory > b.Memory ){
+                        if ( a.Memory < b.Memory ){
                           return -1;
                         }
-                        if ( a.Memory < b.Memory ){
+                        if ( a.Memory > b.Memory ){
                           return 1;
                         }
                         return 0;
@@ -267,15 +257,15 @@ function sortProduct(){
 
 
                 if(option=="Memory"){
-                    function compare( a, b ) {
-                        if ( a.Memory < b.Memory ){
-                          return -1;
-                        }
-                        if ( a.Memory > b.Memory ){
-                          return 1;
-                        }
-                        return 0;
-                      }
+                  function compare( a, b ) {
+                    if ( a.Memory < b.Memory ){
+                      return 1;
+                    }
+                    if ( a.Memory > b.Memory ){
+                      return -1;
+                    }
+                    return 0;
+                  }
                       product.sort( compare );
                     var row = table3.insertRow();
                     var cell1 = row.insertCell(0);
@@ -316,4 +306,112 @@ function sortProduct(){
             
         
     }
-}
+  }
+
+
+  function addProduct(){
+    var Table = document.getElementById("table4");
+    Table.innerHTML = "";
+
+    var com = document.getElementById("Company").value;  
+  
+    var mod = document.getElementById("Model").value;
+
+    var mem = document.getElementById("Memory").value;  
+  
+    var pri = document.getElementById("Price").value;
+
+   
+    /*
+    k=product.length;
+    product[k].Company=com;
+    product[k].Model=mod;
+    product[k].Memory=mem;
+    product[k].Price=pri;*/
+    if(!(com=='' && mod=='' && mem=='' && pri==''))
+    {
+    product.push({Company:com,Model:mod,Memory:mem,Price:pri});
+    }
+    
+    for(var i=0;i<=product.length;){
+      var row = table4.insertRow();
+      
+          var cell1 = row.insertCell(0);
+          cell1.innerHTML = product[i].Company;
+          var cell2 = row.insertCell(1);
+          cell2.innerHTML = product[i].Model;
+          var cell3 = row.insertCell(2);
+          cell3.innerHTML = product[i].Memory;
+          var cell4 = row.insertCell(3);
+          cell4.innerHTML = product[i].Price;
+          i++;
+      
+  }
+   
+
+    /*
+    var f = document.getElementById("filter").value;  
+  
+    var s = document.getElementById("search").value;
+
+    for(var i=0;i<=product.length;i++){
+          if(f=="Company"){
+              if(product[i].Company==s){
+                  var row = table2.insertRow();
+                  var cell1 = row.insertCell(0);
+                  cell1.innerHTML = product[i].Company;
+                  var cell2 = row.insertCell(1);
+                  cell2.innerHTML = product[i].Model;
+                  var cell3 = row.insertCell(2);
+                  cell3.innerHTML = product[i].Memory;
+                  var cell4 = row.insertCell(3);
+                  cell4.innerHTML = product[i].Price;
+                  
+              }
+          }
+          if(f=="Model"){
+              if(product[i].Model==s){
+                  var row = table2.insertRow();
+                  var cell1 = row.insertCell(0);
+                  cell1.innerHTML = product[i].Company;
+                  var cell2 = row.insertCell(1);
+                  cell2.innerHTML = product[i].Model;
+                  var cell3 = row.insertCell(2);
+                  cell3.innerHTML = product[i].Memory;
+                  var cell4 = row.insertCell(3);
+                  cell4.innerHTML = product[i].Price;
+                  
+              }
+          }
+          if(f=="Memory"){
+              if(product[i].Memory==s){
+                  var row = table2.insertRow();
+                  var cell1 = row.insertCell(0);
+                  cell1.innerHTML = product[i].Company;
+                  var cell2 = row.insertCell(1);
+                  cell2.innerHTML = product[i].Model;
+                  var cell3 = row.insertCell(2);
+                  cell3.innerHTML = product[i].Memory;
+                  var cell4 = row.insertCell(3);
+                  cell4.innerHTML = product[i].Price;
+                  
+              }
+          }
+          if(f=="Price"){
+              if(product[i].Price==s){
+                  var row = table2.insertRow();
+                  var cell1 = row.insertCell(0);
+                  cell1.innerHTML = product[i].Company;
+                  var cell2 = row.insertCell(1);
+                  cell2.innerHTML = product[i].Model;
+                  var cell3 = row.insertCell(2);
+                  cell3.innerHTML = product[i].Memory;
+                  var cell4 = row.insertCell(3);
+                  cell4.innerHTML = product[i].Price;
+                  
+                }
+            }
+          
+      
+      }*/
+  }
