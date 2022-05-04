be15d83fecbf6bfb4572e93272a050571d0860c9
sachin2022apr-cedcommerce <sachin2022apr@cedcoss.com>
2022-05-04 14:52:38 +0530

    Stand in Line

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
-// Setup
-let processed = 0;
-
-function processArg(num) {
-  return (num + 3) / 5;
+function nextInLine(arr, item) {
+  // Only change code below this line
+  if(arr.length>0)
+    item = arr[item-1];
+  testArr[4] = 10;
+  return item;
+  // Only change code above this line
 }
 
-// Only change code below this line
-processed = processArg(7);
+// Setup
+const testArr = [1, 2, 3, 4, 5];
+
+// Display code
+console.log("Before: " + JSON.stringify(testArr));
+console.log(nextInLine(testArr, 6));
+console.log("After: " + JSON.stringify(testArr));
