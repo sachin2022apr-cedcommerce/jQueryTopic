0221db9caaa60bbcb395d289db657e58582718d7
sachin2022apr-cedcommerce <sachin2022apr@cedcoss.com>
2022-05-04 17:34:09 +0530

    Multiple Identical Options in Switch Statements

--- script.js
+++ script.js
@@ -1,19 +1,28 @@
-function switchOfStuff(val) {
-  var answer = "";
-  switch (val) {
-    case "a":
-      answer = "apple";
-      break;
-    case "b":
-      answer = "bird";
-      break;
-    case "c":
-      answer = "cat";
-      break;
-    default:
-      answer = "stuff";
-      break;
-  }
-    return answer;  
-  }
-switchOfStuff(1);
+function sequentialSizes(val) {
+  let answer = "";
+  // Only change code below this line
+
+switch(val) {
+  case 1:
+  case 2:
+  case 3:
+    answer = "Low";
+    break;
+  case 4:
+  case 5:
+  case 6:
+    answer = "Mid";
+    break;
+  case 7:
+  case 8:
+  case 9:
+    answer = "High";
+    break;
+}
+
+
+  // Only change code above this line
+  return answer;
+}
+
+sequentialSizes(1);
