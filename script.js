2f661a9b25f828af60cd27501b6a32515fd1067a
sachin2022apr-cedcommerce <sachin2022apr@cedcoss.com>
2022-05-04 12:21:30 +0530

    Understanding Uninitialized Variables

--- script.js
+++ script.js
@@ -1,2 +1,11 @@
-var myFirstName = "Sachin";
-var myLastName = "kush";
+// Only change code below this line
+var a;
+var b;
+var c;
+// Only change code above this line
+a = 5;
+b = 10;
+a = a + 1;
+b = b + 5;
+c = "I am a"
+c = c + " String!";
