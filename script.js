2adba2ddf8418ab04e6ad7a3247540cc7f8834ed
sachin2022apr-cedcommerce <sachin2022apr@cedcoss.com>
2022-05-04 13:11:45 +0530

    Appending Variables to Strings

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// Only change code below this line
-const myName = "Sachin";
-const myStr = "My name is " + myName + " and I am well!";
-
+// Change code below this line
+const someAdjective = "awesome!";
+let myStr = "Learning to code is ";
+myStr += someAdjective;
