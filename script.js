6f0b14e3ffd1e85ec33fa7566043e300810b7a5b
sachin2022apr-cedcommerce <sachin2022apr@cedcoss.com>
2022-05-04 19:04:21 +0530

    Generate Random Fractions with JavaScript

--- script.js
+++ script.js
@@ -1,42 +1,12 @@
-// Setup
-const contacts = [
-  {
-    firstName: "Akira",
-    lastName: "Laine",
-    number: "0543236543",
-    likes: ["Pizza", "Coding", "Brownie Points"],
-  },
-  {
-    firstName: "Harry",
-    lastName: "Potter",
-    number: "0994372684",
-    likes: ["Hogwarts", "Magic", "Hagrid"],
-  },
-  {
-    firstName: "Sherlock",
-    lastName: "Holmes",
-    number: "0487345643",
-    likes: ["Intriguing Cases", "Violin"],
-  },
-  {
-    firstName: "Kristian",
-    lastName: "Vos",
-    number: "unknown",
-    likes: ["JavaScript", "Gaming", "Foxes"],
-  },
-];
-
-function lookUpProfile(name, prop) {
-  for (let i = 0; i < contacts.length; i++) {
-    if (contacts[i].firstName === name) {
-      if (prop in contacts[i]) {
-        return contacts[i][prop];
-      } else {
-        return "No such property";
-      }
-    }
+function randomFraction() {
+  // Only change code below this line.
+  var result = 0;
+  // Math.random() can generate 0. We don't want to     return a 0,
+  // so keep generating random numbers until we get one     that isn't 0
+  while (result === 0) {
+    result = Math.random();
   }
-  return "No such contact";
-}
 
-lookUpProfile("Akira", "likes");
+  return result;
+  // Only change code above this line.
+}
