5650506f465385d90d5c620f826b10a09af8f34d
sachin2022apr-cedcommerce <sachin2022apr@cedcoss.com>
2022-05-04 18:32:43 +0530

    Accessing Nested Objects

--- script.js
+++ script.js
@@ -1,23 +1,16 @@
-var myMusic = [
-  {
-    "artist": "Billy Joel",
-    "title": "Piano Man",
-    "release_year": 1973,
-    "formats": [ 
-      "CD",
-      "8T",
-      "LP"
-    ],
-    "gold": true
-  }, 
-  // Add record here
-  {
-    "artist": "Beau Carnes",
-    "title": "Cereal Man",
-    "release_year": 2003,
-    "formats": [
-      "YouTube video","Videos"
-    ]
+// Setup
+var myStorage = {
+  "car": {
+    "inside": {
+      "glove box": "maps",
+      "passenger seat": "crumbs"
+     },
+    "outside": {
+      "trunk": "jack"
+    }
   }
-];
+};
 
+var gloveBoxContents = myStorage.car.inside["glove box"]; // Change this line
+
+console.log(gloveBoxContents)
