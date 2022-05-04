d06b535c1cb02b8d2c76075e5565ca0360a923ec
sachin2022apr-cedcommerce <sachin2022apr@cedcoss.com>
2022-05-04 17:26:24 +0530

    Selecting from Many Options with Switch Statements

--- script.js
+++ script.js
@@ -1,20 +1,23 @@
-var names = ["Hole-in-one!", "Eagle", "Birdie", "Par", "Bogey", "Double Bogey", "Go Home!"];
-function golfScore(par, strokes) {
-  if (strokes == 1) {
-    return names[0]
-  } else if (strokes <= par - 2) {
-    return names[1]
-  } else if (strokes == par - 1) {
-    return names[2]
-  } else if (strokes == par) {
-    return names[3]
-  } else if (strokes == par + 1) {
-    return names[4]
-  } else if (strokes == par + 2) {
-    return names[5]
-  } else if (strokes >= par + 3) {
-    return names[6]
+function caseInSwitch(val) {
+  var answer = "";
+  switch(val) {
+    case 1:
+      answer = "alpha";
+      break;
+    case 2:
+      answer = "beta";
+      break;
+    case 3:
+      answer = "gamma";
+      break;
+    case 4:
+      answer = "delta";
+      break;
   }
+
+  return answer;  
 }
 
-console.log(golfScore(5, 4));
+
+
+caseInSwitch(1);
