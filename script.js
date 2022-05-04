16b8256fa1bca7faa5905eff19e3a67f9dd669bc
sachin2022apr-cedcommerce <sachin2022apr@cedcoss.com>
2022-05-04 14:19:34 +0530

    Write Reusable JavaScript with Functions

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
-let myList = [];
-myList[0] = ["Chocolate Bar", 15];
-myList[1] = ["Chocolate Bar", 15];
-myList[2] = ["Chocolate Bar", 15];
-myList[3] = ["Chocolate Bar", 15];
-myList[4] = ["Chocolate Bar", 15];
+function reusableFunction() {
+  console.log("Hi World");
+}
+reusableFunction();
