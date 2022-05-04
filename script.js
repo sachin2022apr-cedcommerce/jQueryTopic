1ada733e7c8652d4bd25502e15f2f396fcb5dab3
sachin2022apr-cedcommerce <sachin2022apr@cedcoss.com>
2022-05-04 18:00:49 +0530

    Updating Object Properties

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 // Setup
-const testObj = {
-  12: "Namath",
-  16: "Montana",
-  19: "Unitas"
+const myDog = {
+  "name": "Coder",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["freeCodeCamp Campers"]
 };
 
 // Only change code below this line
-var playerNumber = 16; // Change this Line
-var player = testObj[playerNumber]; // Change this Line
+myDog.name="Happy Coder";
