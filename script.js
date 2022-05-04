81b29b7f2e4e7c23b26785f9a6c38a754ef994c5
sachin2022apr-cedcommerce <sachin2022apr@cedcoss.com>
2022-05-04 18:31:22 +0530

    Manipulating Complex Objects

--- script.js
+++ script.js
@@ -1,7 +1,23 @@
-function checkObj(obj, checkProp) {
-  if (obj.hasOwnProperty(checkProp)) {
-    return obj[checkProp];
-  } else {
-    return "Not Found";
+var myMusic = [
+  {
+    "artist": "Billy Joel",
+    "title": "Piano Man",
+    "release_year": 1973,
+    "formats": [ 
+      "CD",
+      "8T",
+      "LP"
+    ],
+    "gold": true
+  }, 
+  // Add record here
+  {
+    "artist": "Beau Carnes",
+    "title": "Cereal Man",
+    "release_year": 2003,
+    "formats": [
+      "YouTube video","Videos"
+    ]
   }
-}
+];
+
