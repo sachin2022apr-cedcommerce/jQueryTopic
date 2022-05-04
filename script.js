2236098f43d3041e90a1404b010bf976b0448693
sachin2022apr-cedcommerce <sachin2022apr@cedcoss.com>
2022-05-04 19:06:50 +0530

    Generate Random Whole Numbers within a Range

--- script.js
+++ script.js
@@ -1,6 +1,3 @@
-var randomNumberBetween0and19 = Math.floor(Math.random() * 20);
-
-function randomWholeNum() {
-  // Only change code below this line.
-  return Math.floor(Math.random() * 10);
+function randomRange(myMin, myMax) {
+  return Math.floor(Math.random() * (myMax - myMin + 1) + myMin);
 }
