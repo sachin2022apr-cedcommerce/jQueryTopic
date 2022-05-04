69f5d945f0b2a67aca4137f08380e90c9b083bb3
sachin2022apr-cedcommerce <sachin2022apr@cedcoss.com>
2022-05-04 14:55:20 +0530

    Understanding Boolean Values

--- script.js
+++ script.js
@@ -1,16 +1,7 @@
-function nextInLine(arr, item) {
+function welcomeToBooleans() {
   // Only change code below this line
-  if(arr.length>0)
-    item = arr[item-1];
-  testArr[4] = 10;
-  return item;
+
+  return true; // Change this line
+
   // Only change code above this line
 }
-
-// Setup
-const testArr = [1, 2, 3, 4, 5];
-
-// Display code
-console.log("Before: " + JSON.stringify(testArr));
-console.log(nextInLine(testArr, 6));
-console.log("After: " + JSON.stringify(testArr));
